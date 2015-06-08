25fe489d420ada091140e9062cce83cb5f28edae
lhaggarty <lhaggarty@me.com>
2015-06-08 16:09:25 -0400

    June 8 change 1

--- sliderAddition.php
+++ sliderAddition.php
@@ -6,17 +6,14 @@
 			<meta name="keywords" content="Hot Potato, YouTube, video, editor, edits, sharing, cut, personalize, render, free, online">
 			<link rel="stylesheet" type="text/css" href="css/style.css"/>
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-			
 			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" ></script>
-	        
-	        
 			<!-- Latest compiled and minified JavaScript -->
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-			
 			<!-- <script src="https://apis.google.com/js/client.js"></script>-->
 			<script src="http://jwpsrv.com/library/F3JbossrEeSDgg4AfQhyIQ.js"></script>
-			
-			
+		    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+		    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 			
 			<link rel="apple-touch-icon" sizes="57x57" href="css/favicon/apple-icon-57x57.png">
 			<link rel="apple-touch-icon" sizes="60x60" href="css/favicon/apple-icon-60x60.png">
@@ -87,20 +84,24 @@
 			</div>
 			</div>
 			<script>
-			var currentCut=0;
-			var totalCutNumber=0;
+			
 			var ProgressBarBefore;
 			var ProgressBarDuring;
 			var ProgressBarAfter;
 			var userEditCounter = 0;
-			var inPoint = [];
-			var outPoint = [];
+			var inPoint = 0;
+			var outPoint;
 			var vidDuration;
 			var videoLoadSwitch = 0;
+			var sliderIn;
+			var sliderOut;
 			
 			var shortYTLink;
 			var tempOutputYoutubeLink;
 			var inputYTLink = "https://www.youtube.com/watch?v=yJDRop2ocFo";
+			
+			var realVal;
+			var videoProgress;
 			
 		    jwplayer("videoPlaybackFrame").setup({
 		        file: inputYTLink,
@@ -113,35 +114,26 @@
 			};
 			
 			function initiateLiveVideoPlayer(){
-				if (inputYTLink == "https://www.youtube.com/watch?v=yJDRop2ocFo"){
-					shortYTLink = inputYTLink;
-					
-				}else if (inputYTLink != "https://www.youtube.com/watch?v=yJDRop2ocFo") {
-					shortYTLink = getQueryVariable("v",$( '#InputYouTubeLink' ).val());
-					inputYTLink = ( "https://www.youtube.com/watch?v=" + shortYTLink);
-					jwplayer().load([{file:inputYTLink}]);
-					tempOutputYoutubeLink = ( "https://www.youtube.com/v/" + shortYTLink);
-				}
-				
+				shortYTLink = getQueryVariable("v",$( '#InputYouTubeLink' ).val());
+				inputYTLink = ( "https://www.youtube.com/watch?v=" + shortYTLink);
 				document.getElementById('ytVidCode').value=shortYTLink;
-		
-				
+			
+				tempOutputYoutubeLink = ( "https://www.youtube.com/v/" + shortYTLink);
 				videoLoadSwitch=1;
-			    
-				// outPoint = jwplayer("videoPlaybackFrame").getDuration();
-// 				vidDuration = jwplayer("videoPlaybackFrame").getDuration();
+			    jwplayer().load([{file:inputYTLink}]);
 				
 				document.getElementById('loadVideoButton').style.display ="none";
 				document.getElementById("playPauseSpan").className="glyphicon glyphicon-pause";
 				document.getElementById('playPauseButton').style.display ="";
 				document.getElementById('videoTimeline').style.display="";
-				document.getElementById('editVis').style.width ="";
-				document.getElementById('postOutVis').style.width ="";
+				document.getElementById('videoProgressBar').style.width ="";
 				document.getElementById('inputRowOne').style.display="none";
 				document.getElementById('changeVideoButton').style.display="";
 				document.getElementById('videoInPoint').disabled=false;
 				userEditCounter=0;
 				jwplayer("videoPlaybackFrame").play();
+				sliderOut=245;
+				sliderIn=1;
 			};
 			
 			function playPauseVideo(){
@@ -151,96 +143,89 @@
 			} else if (jwplayer("videoPlaybackFrame").getState()=="PAUSED" || jwplayer("videoPlaybackFrame").getState()=="BUFFERING"){
 				document.getElementById("playPauseSpan").className="glyphicon glyphicon-pause";
 			}
-			};
+			
+		};
 			function videoInPoint(){
-				inPoint[currentCut] = jwplayer("videoPlaybackFrame").getPosition();
-				
+				inPoint = jwplayer("videoPlaybackFrame").getPosition();
 				videoLoadSwitch=1;
 				userEditCounter=1;
-				document.getElementById("InTimeCode").value=inPoint[currentCut];
+				document.getElementById("InTimeCode").value=inPoint;
 				document.getElementById('videoOutPoint').disabled=false;
 			};
 			
 			function videoOutPoint(){
-				outPoint[currentCut] = jwplayer("videoPlaybackFrame").getPosition();
+				outPoint= jwplayer("videoPlaybackFrame").getPosition();
 				videoLoadSwitch=1;
 				userEditCounter=2;
-				document.getElementById("OutTimeCode").value=outPoint[currentCut];
+				document.getElementById("OutTimeCode").value=outPoint;
 				document.getElementById('previewEdit').disabled=false;
-				document.getElementById('addEdit').style.display="";
 			};
 			
 			
 			jwplayer("videoPlaybackFrame").onTime(function (event){
-				var realVal = event.position;
+				realVal = event.position;
 				vidDuration = jwplayer("videoPlaybackFrame").getDuration();
-				var videoProgress;
+				videoProgress = (realVal/vidDuration);
+				videoProgress = videoProgress*100;
+				videoProgress = videoProgress.toPrecision(2);
+				document.getElementById('videoProgressBar').style.width =(videoProgress+"%");
 				
-				
-				
-				if (userEditCounter==0){
-				ProgressBarBefore = (realVal/vidDuration);
-				ProgressBarBefore = ProgressBarBefore*100;
-				ProgressBarBefore = ProgressBarBefore.toPrecision(2);
-				
-				document.getElementById('preInVis').style.width =(ProgressBarBefore+"%");
-				
-				} else if (userEditCounter==1){
-					ProgressBarBefore = ((inPoint[currentCut])/vidDuration);
-					ProgressBarBefore = ProgressBarBefore*100;
-					ProgressBarBefore = ProgressBarBefore.toPrecision(2);
-					document.getElementById('preInVis').style.width =(ProgressBarBefore+"%");
+				$( "#slider-range" ).slider({
+				  range: true,
+				  min: 0,
+				  max: vidDuration,
+				  values: [ sliderIn, sliderOut ],
+				  slide: function( event, ui ) {
+				    $( "#sliderAmount" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+				  }
+				});
+				$( "#sliderAmount" ).val($( "#slider-range" ).slider( "values", 0 ) +
+				  " - " + $( "#slider-range" ).slider( "values", 1 ) );   
+							
+				  
+				if (sliderIn < (realVal-15) || $( "#slider-range" ).slider( "values", 0 ) > (realVal+15) && userEditCounter == 0){
 					
-					ProgressBarDuring = ((realVal-inPoint[currentCut])/vidDuration);
-					ProgressBarDuring = ProgressBarDuring*100;
-					ProgressBarDuring = ProgressBarDuring.toPrecision(2);
-					document.getElementById('editVis').style.width =(ProgressBarDuring+"%");
+					jwplayer("videoPlaybackFrame").seek($( "#slider-range" ).slider( "values", 0 ));
+					inPoint = sliderIn;
+					userEditCounter=1;
+				} 
+				else if (sliderOut < (realVal-15) || sliderOut > (realVal+15) && sliderOut != 245 && userEditCounter == 1){
 					
-					document.getElementById('postOutVis').style.width ="";
-				} else if (userEditCounter==2){
-				ProgressBarDuring = ((outPoint[currentCut]-inPoint[currentCut])/vidDuration);
-				ProgressBarDuring = ProgressBarDuring*100;
-				ProgressBarDuring = ProgressBarDuring.toPrecision(2);
-				document.getElementById('editVis').style.width =(ProgressBarDuring+"%");
-				
-				ProgressBarAfter = ((realVal-outPoint[currentCut])/vidDuration);
-				ProgressBarAfter = ProgressBarAfter*100;
-				ProgressBarAfter = ProgressBarAfter.toPrecision(2);
-				//$('#OutputYouTubeLink').html(progressBar1);
-				document.getElementById('postOutVis').style.width =(ProgressBarAfter+"%");
-				}
-			});
+					jwplayer("videoPlaybackFrame").seek(sliderIn);
+					outPoint=sliderOut;
+					userEditCounter=2;
+					
+				} // else if (sliderIn < inPoint || sliderIn > inPoint && sliderIn != realVal){
+// 					inPoint=sliderIn;
+// 					userEditCounter=1;
+// 				} else if (sliderOut < outPoint || sliderOut > outPoint && sliderOut != vidDuration){
+// 					outPoint=sliderOut;
+// 					userEditCounter=2;
+// 				}
+				// else if (userEditCounter==0){
+// 					sliderIn = realVal;
+//
+// 				} else if (userEditCounter==1){
+// 					sliderIn = inPoint;
+// 					sliderOut = realVal;
+// 				} else if (userEditCounter==2){
+// 					slideOut = outPoint;
+//
+// 				}
+			 
+});
+			
 			if (videoLoadSwitch==5){
-			if (realVal < inPoint[currentCut] || realVal > outPoint[currentCut]){
-				jwplayer("videoPlaybackFrame").seek(inPoint[currentCut]);
+			if (realVal < inPoint || realVal > outPoint){
+				jwplayer("videoPlaybackFrame").seek(inPoint);
 				
 			}
 			}
-			if (videoLoadSwitch==2){
-				jwplayer("videoPlaybackFrame").seek(outPoint[currentCut-1]);
-				videoLoadSwitch=1;
-			}
-			if (videoLoadSwitch==-1){
-				jwplayer("videoPlaybackFrame").seek(inPoint[currentCut]);
-				videoLoadSwitch=1;
-			}
-			</script>	
-  		<div class="row">
-  		<div class="col-md-8 col-md-offset-3" style="padding-right:21.75%">
-  			<div class="progress" id="videoTimeline" style="">	  
-  		  <div class="progress-bar progress-bar-info" id="preInVis" role="progressbar" style="">
-  		</div>	
-  		  <div class="progress-bar progress-bar-warning progress-bar-striped" id="editVis" role="progressbar" style="">
-  			</div>
-    		  <div class="progress-bar progress-bar-info" id="postOutVis" role="progressbar" style="">
-    			</div>
-  		</div>
-  		  </div>
-  	  </div>
-	  
+		    
+			</script>
 		<div class="row">
 		
-		<div class="col-md-8 col-md-offset-3" style="padding-right:21.75%" id="inputRowOne"> 
+		<div class="col-md-8 col-md-offset-3" style="padding-top: 1%; padding-right:21.75%" id="inputRowOne"> 
 		<form role="form" action="linkBoi.php" method="get">
 			<div class="form-group">
 				
@@ -255,15 +240,33 @@
 		</div>
 	</div>
 		
-		<div class="col-md-8 col-md-offset-3" id="videoPlayerButtons" >
+		
+		<div class="col-md-8 col-md-offset-3" style="padding-right:21.75%">
 		<div class="row">
+			<div class="progress" id="videoTimeline" style="display:none">	  
+		  <div class="progress-bar progress-bar-info" id="videoProgressBar" role="progressbar" style="">
+		</div>
+		</div>
+		</div>
+		<div class="row">
+		<p>
+		  <label for="sliderAmount">Timecode:</label>
+		  <input type="text" id="sliderAmount" readonly style="border:0; color:#f6931f; font-weight:bold;">
+		</p>
+ 
+		<div id="slider-range"></div>
+		  </div>
+	  </div>
+		
+		<div class="row">
+			<div class="col-md-8 col-md-offset-3" id="videoPlayerButtons" >
 				<button type="button" class="btn btn-default btn-lg" id="loadVideoButton" onclick="initiateLiveVideoPlayer();">
 					<span class="glyphicon glyphicon-search" aria-hidden="true"> </span>
 					<span> LOAD</span>
 				</button>
 				
 				<button type="button" class="btn btn-default btn-lg" id="playPauseButton" onclick="playPauseVideo();" style="display:none">
-					<span class="glyphicon glyphicon-play" id="playPauseSpan" aria-hidden="true"></span>
+					<span class="glyphicon glyphicon-pause" id="playPauseSpan" aria-hidden="true"></span>
 				</button>
 				<button type="button" class="btn btn-default btn-lg" id="videoInPoint" onclick="videoInPoint()" disabled>
 					<span class="glyphicon glyphicon-scissors" aria-hidden="true"> </span>
@@ -283,39 +286,15 @@
 					<span> SUBMIT</span>
 				</button>
 			
+			
 			<button type="button" class="btn btn-default btn-lg" id="changeVideoButton" onclick="reloadVideoInput()" style="display:none">
 				<span class="glyphicon glyphicon-repeat" aria-hidden="true"> </span>
 					<span> Change Video</span>
-				</button>
-			
-				<button type="button" class="btn btn-default btn-lg" id="addEdit" onclick="addEdit();" style="display:none">
-				<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-				<span> CUT</span>
-				</button>
+				</button>	
 			</div>
-			<div class="row" id="editTimelineButtons" style="display:none">
-			<button type="button" class="btn btn-default" id="playbackAllEdits" onclick="playbackAllEdits();" style="">
-			<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-			<span> PLAY ALL</span>
-			</button>
-			<button type="button" class="btn btn-default" id="previousEdit" onclick="previousEdit();" style="">
-			<span class="glyphicon glyphicon-import" aria-hidden="true"></span>
-			<span id="previousEditText"> PREVIOUS CUT</span>
-			</button>
-			<button type="button" class="btn btn-default" id="nextEdit" onclick="nextEdit();" style="">
-			<span class="glyphicon glyphicon-export" aria-hidden="true"></span>
-			<span id="nextEditText"> NEXT CUT</span>
-			</button>
-			<button type="button" class="btn btn-default" id="lastEdit" onclick="lastEdit();" style="">
-			<span class="glyphicon glyphicon-object-align-right" aria-hidden="true"></span>
-			<span> LAST CUT</span>
-			</button>
 		</div>
-		</div>
-		
 		
 				<div class="col-md-6 col-md-offset-3" style="padding-top: 1%; padding-right:5.5%"> 
-				
 				<div class ="row" >
 						<div class="progress-bar progress-bar-warning progress-bar-striped active" id="outputLoadingBar" role="progressbar" style="width:100%;display:none">EXPORTING
 				</div>
@@ -349,69 +328,33 @@
 				    });
 				});
 				
-			function reloadVideoInput(){
+				function reloadVideoInput(){
 					document.getElementById('InputYouTubeLink').value="";
 					document.getElementById('inputRowOne').style.display="";
 					document.getElementById('InputYouTubeLink').style.display="";
 					document.getElementById('changeVideoButton').style.display="none";
-					document.getElementById('addEdit').style.display="none";
 					document.getElementById('videoTimeline').style.display="none";
 					document.getElementById('videoInPoint').disabled=true;
 					document.getElementById('videoOutPoint').disabled=true;
 					document.getElementById('submitEdit').style.display="none";
 					document.getElementById('previewEdit').style.display="";
 					document.getElementById('previewEdit').disabled=true;
-					inPoint[currentCut] = 0;
-					outPoint[currentCut] = 0;
+					inPoint = 0;
+					outPoint = 0;
 					videoLoadSwitch=1;
-			}
-			function addEdit(){
-					totalCutNumber++;
-					currentCut++;
-					document.getElementById('previousEdit').style.display="";
-					document.getElementById('lastEdit').style.display="";
-					document.getElementById('nextEdit').style.display="";
-					document.getElementById('editVis').style.width="";
-					document.getElementById('postOutVis').style.width="";
-					userEditCounter=0;
-					videoLoadSwitch=2;
-					document.getElementById('editTimelineButtons').style.display="";
-			}
+					jwplayer("videoPlaybackFrame").play();
+				}
+				
 			
-			function previousEdit(){
-				currentCut--;
-				videoLoadSwitch=-1;
-				userEditCounter=1;
-				document.getElementById("previousEditText").innerHTML = (" EDIT"+(currentCut-1));
-				document.getElementById("nextEditText").innerHTML = (" EDIT"+(currentCut+1));
-			}
-			function nextEdit(){
-				currentCut++;
-				videoLoadSwitch=-1;
-				userEditCounter=1;
-				document.getElementById("previousEditText").innerHTML = (" EDIT"+(currentCut-1));
-				document.getElementById("nextEditText").innerHTML = (" EDIT"+(currentCut+1));
-			}
-			function lastEdit(){
-				currentCut=totalCutNumber;
-				videoLoadSwitch=-1;
-				userEditCounter=1;
-			}
 			
 			function previewEdit(){
 				document.getElementById('submitEdit').style.display="";
 				document.getElementById('previewEdit').style.display="none";
 				videoLoadSwitch=5;
 				userEditCounter=1;
-				document.getElementById('postOutVis').style.width="";
+				
 			}
 			function submitEdit(){
-				currentCut=0;
-				while (currentCut < (totalCutNumber+1)){
-					document.getElementById('OutTimeCode').value =($( '#OutTimeCode' ).val(),'"',outPoint[currentCut],'",');
-					document.getElementById('InTimeCode').value =($( '#InTimeCode' ).val(),'"',inPoint[currentCut],'",');
-				}
-				
 				document.getElementById('outputLoadingBar').style.display ="";
 				
 				$('#collapseAltLink').html(tempOutputYoutubeLink);
@@ -473,8 +416,8 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-57874990-4', 'auto');
-  ga('send', 'pageview');
+  ga("create", "UA-57874990-5", "auto");
+  ga("send", "pageview");
 
 </script>
 </html> 
