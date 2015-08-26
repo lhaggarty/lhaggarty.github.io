9f6033ed9f6fb05a9e68364cd568e6987adfa067
lhaggarty <lhaggarty@me.com>
2015-08-26 13:28:45 -0400

    August 26 change 1

--- multiEditTest.php
+++ multiEditTest.php
@@ -6,7 +6,7 @@
 //$db = mysql_select_db('single_edits');
 if($_GET){
     if(isset($_GET['createLink'])){
-        createLink();
+        testingData();
     }
 	elseif (isset($_GET['resubmitLink'])){
 	        postToFeed();
@@ -14,5 +14,14 @@ if($_GET){
 	else {
 		
 	}
+}
+
+function testingData() {
+	
+	$videoStart = array($_GET['InTimeCode']);
+	$videoStopData = ($_GET['OutTimeCode']);
+	$videoStop = parse_str($videoStopData);
+	$ytVidCode = $_GET['ytVidCode'];
+	var_dump($videoStop);
 }
 ?>
